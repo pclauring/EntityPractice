a88432bfe2616ede527770b81fa54427c64e8a45
Pierce C Lauring <pclauring@gmail.com>
2017-11-20 21:33:02 -0500

    First Commit

---
+++ EntityPractice/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using EntityPractice.Models;
+
+namespace EntityPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ListCustomers()
+        {
+            //creating object from the ORM
+            NorthwindEntities ORM = new NorthwindEntities();
+
+            //load the data from the DbSet into a data structure
+            List<Customer> CustomerList = ORM.Customers.ToList();
+            //filter the data
+            ViewBag.CustomerList = CustomerList;
+            return View("CustomersView");
+        }
+
+        public ActionResult ListCustomersByCountry(string Country)
+        {
+            NorthwindEntities ORM = new NorthwindEntities();
+
+            List<Customer> OutputList = new List<Customer>();
+            foreach (Customer c in ORM.Customers.ToList())
+            {
+                if (c.Country.ToLower() == Country.ToLower())
+                {
+                    OutputList.Add(c);
+                }
+            }
+
+            ViewBag.CustomerList = OutputList;
+            return View("CustomersView");
+        }
+
+        public ActionResult SearchForCustomerID(string CustomerID)
+        {
+            NorthwindEntities ORM = new NorthwindEntities();
+            List<Customer> OutputList = new List<Customer>();
+
+            foreach (Customer c in ORM.Customers.ToList())
+            {
+                if (c.CustomerID.Contains(CustomerID.ToUpper()))
+                {
+                    OutputList.Add(c);
+                }
+            }
+
+            ViewBag.CustomerList = OutputList;
+            return View("CustomersView");
+        }
+    }
+}
---
+++ EntityPractice/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EntityPractice")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EntityPractice")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("149b9385-945b-4baf-9408-2a04beff3b54")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
